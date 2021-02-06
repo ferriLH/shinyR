ff761f78ce607f4a7a0e2a43bd23447772c058d6
Ferri L Halim <ferrilasmihalim@gmail.com>
2021-02-06 12:00:37 +0700

    run2

--- run.R
+++ run.R
@@ -19,11 +19,11 @@ library(mongolite)
 #     ui = ui,
 #     server = server
 # )
-# shiny::runApp()
+shiny::runApp()
 # CLOUD
 # port <- Sys.getenv('PORT')
-shiny::runApp(
-  appDir = getwd(),
-  host = '0.0.0.0',
-  port = as.numeric(port)
-)
+# shiny::runApp(
+#   appDir = getwd(),
+#   host = '0.0.0.0',
+#   port = as.numeric(port)
+# )
